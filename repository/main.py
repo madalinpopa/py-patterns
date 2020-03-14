48e3ad2d0d14e73936cc496424de08d48c858d88
Madalin Popa <contact@madalinpopa.com>
2020-03-14 18:32:39 +0200

    update repo

--- repository/main.py
+++ repository/main.py
@@ -7,7 +7,7 @@ from sqlalchemy.orm import sessionmaker
 
 from model import Profile, User
 from orm import metadata, start_mapper
-from repo import AbstractRepository, ProfileRepository, UserRepository
+from repo import AbstractRepository, SqlRepository
 
 # define engine
 engine = create_engine("sqlite:///repo.db", echo=True)
@@ -23,21 +23,24 @@ def main():
     # create all tables
     metadata.create_all(bind=engine)
 
+    # initiate repository
+    user_repo = SqlRepository(session)
+
     user = User("ed", "secret")
     profile = Profile("John", "Doe", "john.doe@gmail.com")
 
-    user_repo = UserRepository(session)
-    user_repo.add(user)
-
-    profile_repo = ProfileRepository(session)
-    profile_repo.add(profile)
-
     user.profile = profile
 
-    user = user_repo.get("ed")
-    profile = profile_repo.get(user.id)
+    # add user and profile to database
+    user_repo.add(user)
 
-    print(user, profile)
+    # save
+    session.commit()
+
+    # query database
+    user = user_repo.get(username="ed")
+
+    print(user, user.profile)
 
 
 if __name__ == "__main__":
--- repository/repo.py
+++ repository/repo.py
@@ -20,14 +20,12 @@ class AbstractRepository(abc.ABC):
         raise NotImplementedError
 
 
-class UserRepository(AbstractRepository):
+class SqlRepository(AbstractRepository):
     def __init__(self, session: Session):
         self._session = session
 
     def add(self, user: User) -> None:
         self._session.add(user)
-        self._session.commit()
-        self._session.flush()
 
     def get(self, username: str) -> User:
         return self._session.query(User).filter_by(username=username).first()
@@ -35,18 +33,3 @@ class UserRepository(AbstractRepository):
     def get_all_users(self) -> List[User]:
         return self._session.query(User).all()
 
-
-class ProfileRepository(AbstractRepository):
-    def __init__(self, session: Session):
-        self._session = session
-
-    def add(self, profile: Profile) -> None:
-        self._session.add(profile)
-        self._session.commit()
-        self._session.flush()
-
-    def get(self, user_id: int) -> Profile:
-        return self._session.query(Profile).filter_by(user_id=user_id).first()
-
-    def get_all_profiles(self) -> List[Profile]:
-        return self._session.query(Profile).all()
