87fdef3dfc5046fe331a78177ebc390ebe47a201
Madalin Popa <contact@madalinpopa.com>
2020-03-14 12:28:09 +0200

    add user model.py

--- repository/model.py
+++ repository/model.py
@@ -1,3 +1,21 @@
 # coding: utf-8
 
 # repository/model.py
+
+from dataclasses import dataclass
+
+
+@dataclass(frozen=True)
+class Profile:
+    firstname: str
+    lastname: str
+    email: str
+    user: User
+
+
+class User:
+    def __init__(self, username: str, password: str, profile: Profile = None):
+        self.username = username
+        self.password = password
+        self.profile = profile
+
