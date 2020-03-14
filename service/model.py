f7f58cb85a7499383ee52dd38408c082f1435932
Madalin Popa <contact@madalinpopa.com>
2020-03-14 23:25:38 +0200

    add service model

--- service/model.py
+++ service/model.py
@@ -9,15 +9,17 @@ from typing import List
 class Blog:
     def __init__(self, name: str):
         self.name = name
-        self.posts = List[Post]
+        self.posts : List[Post]
 
 
 class Post:
     def __init__(self, title: str, date: datetime.datetime):
         self.title = title
         self.date = date
+        self.tags: List[Tag]
 
 
 class Tag:
     def __init__(self, desc: str):
         self.desc = desc
+        self.posts : List[Post]
