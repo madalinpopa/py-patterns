5591cad4fc07ff40cc26de51648f800528645b56
Madalin Popa <contact@madalinpopa.com>
2020-03-14 21:14:11 +0200

    finis uow

--- uow/unit_of_work.py
+++ uow/unit_of_work.py
@@ -3,7 +3,45 @@
 # uow/unit_of_work.py
 
 import abc
+from repo import AbstractRepository, SqlRepository
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy import create_engine
 
+
+DEFAULT_SESSION = sessionmaker(bind=create_engine("sqlite:///uow.db"))
 
 class AbstractUnitOfWork(abc.ABC):
-    pass
+    
+    repo = AbstractRepository
+
+    def __exit__(self, *args):
+        self.rollback()
+
+    @abc.abstractmethod
+    def commit(self):
+        raise NotImplementedError
+
+    @abc.abstractmethod
+    def rollback(self):
+        raise NotImplementedError
+
+
+class SqlAlchemyUnitOfWork(AbstractRepository):
+
+    def __init__(self, session_factory=DEFAULT_SESSION):
+        self.session_factory = session_factory
+
+    def __enter__(self):
+        self.session = self.session_factory()
+        self.repo = SqlRepository(self.session)
+        return super().__enter__()
+
+    def __exit__(self, *args):
+        super().__exit__(*args)
+        self.session.close()
+
+    def commit(self):
+        self.session.commit()
+
+    def rollback(self):
+        self.session.rollback()
