779110a6ca237fdfad05d53d067a00c4dc611587
Madalin Popa <contact@madalinpopa.com>
2020-03-16 09:30:41 +0200

    code clean

--- aggregates/model.py
+++ aggregates/model.py
@@ -3,6 +3,18 @@
 # aggregates/model.py
 
 
+class Authentication:
+
+    def __init__(self, password: str)
+        self._user = User
+
+    def hash_user_pass(self):
+        pass
+
+    def verify_user_pass(self):
+        pass
+
+
 class User:
     def __init__(self, username: str, password: str):
         self.username = username
