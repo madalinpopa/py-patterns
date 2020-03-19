53107d52a334f2e67323bb570ddd61b4db98fe82
Madalin Popa <contact@madalinpopa.com>
2020-03-19 18:11:08 +0200

    update user

--- aggregates/model.py
+++ aggregates/model.py
@@ -2,17 +2,35 @@
 
 # aggregates/model.py
 
+import binascii
+import hashlib
+import os
+from typing import List
 
-class Authentication:
 
-    def __init__(self, password: str)
-        self._user = User
+class UserManagement:
+    def __init__(self, username: str, password: str, roles: List[str]):
+        self.username = username
+        self.password = password
+        self.roles = roles
 
-    def hash_user_pass(self):
-        pass
 
-    def verify_user_pass(self):
-        pass
+    def create_user
+
+    def _hash_user_pass(self, password):
+        salt = hashlib.sha256(os.urandom(60)).hexdigest().encode("ascii")
+        pwdhash = hashlib.pbkdf2_hmac("sha512", password.encode("utf-8"), salt, 100000)
+        pwdhash = binascii.hexlify(pwdhash)
+        return (salt + pwdhash).decode("ascii")
+
+    def _verify_user_pass(self, stored_password, provided_password):
+        salt = stored_password[:64]
+        stored_password = stored_password[64:]
+        pwdhash = hashlib.pbkdf2_hmac(
+            "sha512", provided_password.encode("utf-8"), salt.encode("ascii"), 1000
+        )
+        pwdhash = binascii.hexlify(pwdhash).decode("ascii")
+        return stored_password == pwdhash
 
 
 class User:
